f8420727793769acee633d272a98a680d0c84b9a
Vladimir Lysenko <morgota199@gmai.com>
2024-01-17 20:28:06 +0200

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: false },
+  generate: {
+    nojekyll: true,
+  },
+  ssr: true,
+  target: 'static',
+  app: {
+    baseURL: '/'
+  }
+})
