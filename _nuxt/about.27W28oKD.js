d0fb09c579db56eda0d74958e167cd468e059965
morgota199 <55238829+morgota199@users.noreply.github.com>
2024-02-08 15:22:44 +0000

    Deploying to gh-pages from @ morgota199/morgota199.github.io@20046c09bb6ce0ae376d3b152228b48a451f9ea9 🚀

---
+++ _nuxt/about.27W28oKD.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,a,t as s,o as c}from"./entry.5ijeGxx9.js";const n={},r={id:"about"};function _(t,i){return c(),o("section",r,[a("h1",null,s(t.$t("about.title")),1)])}const u=e(n,[["render",_]]);export{u as default};
